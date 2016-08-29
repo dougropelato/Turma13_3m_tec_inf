179d1cc7f17507c3db082279b7619e84fde16d95
Erickson Largura <erickson.largura@gmail.com>
2016-08-29 19:00:21 -0300

    Botao
    Botao

--- RemIRC/src/Douglas/NovoJFrame.java
+++ RemIRC/src/Douglas/NovoJFrame.java
@@ -72,7 +72,7 @@ public class NovoJFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        System.exit(0);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
