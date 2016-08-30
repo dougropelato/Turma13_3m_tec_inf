1ed0689fc00ffd72d2e78ddc938341daa6736e2e
Erickson Largura <erickson.largura@gmail.com>
2016-08-29 21:27:52 -0300

    Usuario
    Tabela usuario com atributos

--- RemIRC/src/tabelas/Usuarios.java
+++ RemIRC/src/tabelas/Usuarios.java
@@ -1,14 +1,105 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tabelas;
 
-/**
- *
- * @author erick
- */
 public class Usuarios {
+    
+    int codigo_usuarios;
+    String nome_usuarios;
+    String email_usuarios;
+    String apelido_usuarios;
+    String foto_usuarios;
+    boolean sexo_usuarios;
+    int idade_usuarios;
+    int endereco_usuarios;
+    String humor_usuarios;
+    int codigo_cidade;
+    int estadocivil_usuarios;
+
+    public int getCodigo_usuarios() {
+        return codigo_usuarios;
+    }
+
+    public void setCodigo_usuarios(int codigo_usuarios) {
+        this.codigo_usuarios = codigo_usuarios;
+    }
+
+    public String getNome_usuarios() {
+        return nome_usuarios;
+    }
+
+    public void setNome_usuarios(String nome_usuarios) {
+        this.nome_usuarios = nome_usuarios;
+    }
+
+    public String getEmail_usuarios() {
+        return email_usuarios;
+    }
+
+    public void setEmail_usuarios(String email_usuarios) {
+        this.email_usuarios = email_usuarios;
+    }
+
+    public String getApelido_usuarios() {
+        return apelido_usuarios;
+    }
+
+    public void setApelido_usuarios(String apelido_usuarios) {
+        this.apelido_usuarios = apelido_usuarios;
+    }
+
+    public String getFoto_usuarios() {
+        return foto_usuarios;
+    }
+
+    public void setFoto_usuarios(String foto_usuarios) {
+        this.foto_usuarios = foto_usuarios;
+    }
+
+    public boolean isSexo_usuarios() {
+        return sexo_usuarios;
+    }
+
+    public void setSexo_usuarios(boolean sexo_usuarios) {
+        this.sexo_usuarios = sexo_usuarios;
+    }
+
+    public int getIdade_usuarios() {
+        return idade_usuarios;
+    }
+
+    public void setIdade_usuarios(int idade_usuarios) {
+        this.idade_usuarios = idade_usuarios;
+    }
+
+    public int getEndereco_usuarios() {
+        return endereco_usuarios;
+    }
+
+    public void setEndereco_usuarios(int endereco_usuarios) {
+        this.endereco_usuarios = endereco_usuarios;
+    }
+
+    public String getHumor_usuarios() {
+        return humor_usuarios;
+    }
+
+    public void setHumor_usuarios(String humor_usuarios) {
+        this.humor_usuarios = humor_usuarios;
+    }
+
+    public int getCodigo_cidade() {
+        return codigo_cidade;
+    }
+
+    public void setCodigo_cidade(int codigo_cidade) {
+        this.codigo_cidade = codigo_cidade;
+    }
+
+    public int getEstadocivil_usuarios() {
+        return estadocivil_usuarios;
+    }
+
+    public void setEstadocivil_usuarios(int estadocivil_usuarios) {
+        this.estadocivil_usuarios = estadocivil_usuarios;
+    }
     
 }
