5d8f67ee224f2fe19127a7d362dcf04de61fbbba
Alan Adriano <alanadriano2@hotmail.com>
2016-08-29 21:29:12 -0300

    tabelas

--- RemIRC/src/tabelas/Salas.java
+++ RemIRC/src/tabelas/Salas.java
@@ -11,4 +11,65 @@ package tabelas;
  */
 public class Salas {
     
+   private int codigo_salas;
+   private String nome_salas;
+   private int ip_salas;
+   private int porta_salas;
+
+    /**
+     * @return the codigo_salas
+     */
+    public int getCodigo_salas() {
+        return codigo_salas;
+    }
+
+    /**
+     * @param codigo_salas the codigo_salas to set
+     */
+    public void setCodigo_salas(int codigo_salas) {
+        this.codigo_salas = codigo_salas;
+    }
+
+    /**
+     * @return the nome_salas
+     */
+    public String getNome_salas() {
+        return nome_salas;
+    }
+
+    /**
+     * @param nome_salas the nome_salas to set
+     */
+    public void setNome_salas(String nome_salas) {
+        this.nome_salas = nome_salas;
+    }
+
+    /**
+     * @return the ip_salas
+     */
+    public int getIp_salas() {
+        return ip_salas;
+    }
+
+    /**
+     * @param ip_salas the ip_salas to set
+     */
+    public void setIp_salas(int ip_salas) {
+        this.ip_salas = ip_salas;
+    }
+
+    /**
+     * @return the porta_salas
+     */
+    public int getPorta_salas() {
+        return porta_salas;
+    }
+
+    /**
+     * @param porta_salas the porta_salas to set
+     */
+    public void setPorta_salas(int porta_salas) {
+        this.porta_salas = porta_salas;
+    }
+    
 }
--- RemIRC/src/tabelas/UsuarioSalas.java
+++ RemIRC/src/tabelas/UsuarioSalas.java
@@ -10,5 +10,49 @@ package tabelas;
  * @author M5L2
  */
 public class UsuarioSalas {
-    
+    private int codigo_usuario_salas;
+    private int codigo_usuario;
+    private int codigo_salas;
+
+    /**
+     * @return the codigo_usuario_salas
+     */
+    public int getCodigo_usuario_salas() {
+        return codigo_usuario_salas;
+    }
+
+    /**
+     * @param codigo_usuario_salas the codigo_usuario_salas to set
+     */
+    public void setCodigo_usuario_salas(int codigo_usuario_salas) {
+        this.codigo_usuario_salas = codigo_usuario_salas;
+    }
+
+    /**
+     * @return the codigo_usuario
+     */
+    public int getCodigo_usuario() {
+        return codigo_usuario;
+    }
+
+    /**
+     * @param codigo_usuario the codigo_usuario to set
+     */
+    public void setCodigo_usuario(int codigo_usuario) {
+        this.codigo_usuario = codigo_usuario;
+    }
+
+    /**
+     * @return the codigo_salas
+     */
+    public int getCodigo_salas() {
+        return codigo_salas;
+    }
+
+    /**
+     * @param codigo_salas the codigo_salas to set
+     */
+    public void setCodigo_salas(int codigo_salas) {
+        this.codigo_salas = codigo_salas;
+    }
 }
