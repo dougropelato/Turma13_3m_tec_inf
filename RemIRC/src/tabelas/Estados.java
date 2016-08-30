0479982a52af8e076dcae746e7d7e6109b3f1b7a
Édio Frankenberger <ediofrankenberger@gmail.com>
2016-08-29 21:40:45 -0300

    d

--- RemIRC/src/tabelas/Estados.java
+++ RemIRC/src/tabelas/Estados.java
@@ -10,5 +10,33 @@ package tabelas;
  * @author ediof
  */
 public class Estados {
+    private String Cod_estados;
+    private String Nome_estados;
+    private String Sigla_estados;
+    
+ 
+    public String getCod_estados() {
+        return Cod_estados;
+    }
+
+    public void setCod_estados(String cod_estados) {
+        this.Cod_estados = cod_estados;
+    }
+
+    public String getNome_estados() {
+        return Nome_estados;
+    }
+
+    public void setNome_estados(String nome_estados) {
+        this.Nome_estados = nome_estados;
+    }
+
+    public String getSigla_estados() {
+        return Sigla_estados;
+    }
+
+    public void setSigla_estados(String sigla_estados) {
+        this.Sigla_estados = sigla_estados;
+    }
     
 }
