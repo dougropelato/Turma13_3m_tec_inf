b5613af360db7e0e43946794905b047b1c2f4a70
Jonathandarui <jonathandarui@outlook.com>
2016-08-31 21:03:16 -0300

    Java
    java

---
+++ RemIRC/src/tabelas/Cidades.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabelas;
+
+/**
+ *
+ * @author CEDUP TIMBO
+ */
+public class Cidades {
+    String CodigoCidades;
+    String NomeCidades;
+    String CepCidades;
+    String CodigoCidade;
+}
