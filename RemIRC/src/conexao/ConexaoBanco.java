ab795283fd754ef6e2b780540b9737fd4d5422cc
Fabricio Zarling <fabricio.zarling@outlook.com>
2016-09-12 22:02:49 -0300

    Endereço do Banco
    Atualizado

--- RemIRC/src/conexao/ConexaoBanco.java
+++ RemIRC/src/conexao/ConexaoBanco.java
@@ -14,7 +14,7 @@ public class ConexaoBanco {
     public  static Connection getConexao() throws ClassNotFoundException, SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mysql://localhost/tabelaaaaa", "teste", "");
+            return DriverManager.getConnection("jdbc:mysql://192.168.1.254/remirc", "teste", "");
         }catch(ClassNotFoundException e){
             throw new SQLException(e.getMessage());
         }
