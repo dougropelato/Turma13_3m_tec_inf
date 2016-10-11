3b748ba95bcebd64c908d2f6981d09f1c964b100
Jonathandarui <jonathandarui@outlook.com>
2016-10-10 21:06:12 -0300

    Novas
    Novas atualizações

--- RemIRC/src/tabelas/Cidades.java
+++ RemIRC/src/tabelas/Cidades.java
@@ -10,8 +10,42 @@ package tabelas;
  * @author CEDUP TIMBO
  */
 public class Cidades {
+    
     String CodigoCidades;
     String NomeCidades;
     String CepCidades;
     String CodigoCidade;
+    
+    public String getCodigoCidades() {
+        return CodigoCidades;
+    }
+
+    public void setCodigoCidades(String CodigoCidades) {
+        this.CodigoCidades = CodigoCidades;
+    }
+
+    public String getNomeCidades() {
+        return NomeCidades;
+    }
+
+    public void setNomeCidades(String NomeCidades) {
+        this.NomeCidades = NomeCidades;
+    }
+
+    public String getCepCidades() {
+        return CepCidades;
+    }
+
+    public void setCepCidades(String CepCidades) {
+        this.CepCidades = CepCidades;
+    }
+
+    public String getCodigoCidade() {
+        return CodigoCidade;
+    }
+
+    public void setCodigoCidade(String CodigoCidade) {
+        this.CodigoCidade = CodigoCidade;
+    }
+   
 }
