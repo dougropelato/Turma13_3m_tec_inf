403270ee9b28796f9ea46869d71405618661d184
Erickson Largura <erickson.largura@gmail.com>
2016-10-17 21:11:03 -0200

    Geral

--- RemIRC/src/tabelas/Cidades.java
+++ RemIRC/src/tabelas/Cidades.java
@@ -1,49 +1,7 @@
 package tabelas;
 
 public class Cidades {
-<<<<<<< HEAD
 
-    String CodigoCidades;
-    String NomeCidades;
-    String CepCidades;
-    String CodigoCidade;
-    
-    public String getCodigoCidades() {
-        return CodigoCidades;
-    }
-
-    public void setCodigoCidades(String CodigoCidades) {
-        this.CodigoCidades = CodigoCidades;
-    }
-
-    public String getNomeCidades() {
-        return NomeCidades;
-    }
-
-    public void setNomeCidades(String NomeCidades) {
-        this.NomeCidades = NomeCidades;
-    }
-
-    public String getCepCidades() {
-        return CepCidades;
-    }
-
-    public void setCepCidades(String CepCidades) {
-        this.CepCidades = CepCidades;
-    }
-
-    public String getCodigoCidade() {
-        return CodigoCidade;
-    }
-
-    public void setCodigoCidade(String CodigoCidade) {
-        this.CodigoCidade = CodigoCidade;
-    }
-   
-
-=======
-   
->>>>>>> origin/master
     int Codigo_Cidades;
     String Nome_Cidades;
     int Codigo_Estados;
